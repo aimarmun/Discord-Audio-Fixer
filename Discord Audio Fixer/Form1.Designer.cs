09933f20110226a28bdea653200050f26e5b5f63
Aimar Mundiñano <aimarmundi@outlook.es>
2021-03-14 19:42:14 +0100

    Agregar archivos de proyecto.

---
+++ Discord Audio Fixer/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Discord_Audio_Fixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFix = new System.Windows.Forms.Button();
+            this.buttonDefaults = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.checkBoxFixOnStart = new System.Windows.Forms.CheckBox();
+            this.checkBoxDefaultWhenExit = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonFix
+            // 
+            this.buttonFix.Location = new System.Drawing.Point(12, 258);
+            this.buttonFix.Name = "buttonFix";
+            this.buttonFix.Size = new System.Drawing.Size(172, 56);
+            this.buttonFix.TabIndex = 0;
+            this.buttonFix.Text = "Fix";
+            this.buttonFix.UseVisualStyleBackColor = true;
+            this.buttonFix.Click += new System.EventHandler(this.buttonFix_Click);
+            // 
+            // buttonDefaults
+            // 
+            this.buttonDefaults.Location = new System.Drawing.Point(199, 258);
+            this.buttonDefaults.Name = "buttonDefaults";
+            this.buttonDefaults.Size = new System.Drawing.Size(172, 56);
+            this.buttonDefaults.TabIndex = 0;
+            this.buttonDefaults.Text = "Defaults";
+            this.buttonDefaults.UseVisualStyleBackColor = true;
+            this.buttonDefaults.Click += new System.EventHandler(this.buttonDefaults_Click);
+            // 
+            // label
+            // 
+            this.label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label.Location = new System.Drawing.Point(13, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(358, 194);
+            this.label.TabIndex = 1;
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBoxFixOnStart
+            // 
+            this.checkBoxFixOnStart.AutoSize = true;
+            this.checkBoxFixOnStart.Location = new System.Drawing.Point(13, 233);
+            this.checkBoxFixOnStart.Name = "checkBoxFixOnStart";
+            this.checkBoxFixOnStart.Size = new System.Drawing.Size(163, 19);
+            this.checkBoxFixOnStart.TabIndex = 2;
+            this.checkBoxFixOnStart.Text = "Fix if need and minimiced";
+            this.checkBoxFixOnStart.UseVisualStyleBackColor = true;
+            this.checkBoxFixOnStart.CheckedChanged += new System.EventHandler(this.checkBoxFix_CheckedChanged);
+            // 
+            // checkBoxDefaultWhenExit
+            // 
+            this.checkBoxDefaultWhenExit.AutoSize = true;
+            this.checkBoxDefaultWhenExit.Location = new System.Drawing.Point(199, 233);
+            this.checkBoxDefaultWhenExit.Name = "checkBoxDefaultWhenExit";
+            this.checkBoxDefaultWhenExit.Size = new System.Drawing.Size(164, 19);
+            this.checkBoxDefaultWhenExit.TabIndex = 3;
+            this.checkBoxDefaultWhenExit.Text = "Restore defaults when exit";
+            this.checkBoxDefaultWhenExit.UseVisualStyleBackColor = true;
+            this.checkBoxDefaultWhenExit.CheckedChanged += new System.EventHandler(this.checkBoxDefaultWhenExit_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 333);
+            this.Controls.Add(this.checkBoxDefaultWhenExit);
+            this.Controls.Add(this.checkBoxFixOnStart);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.buttonDefaults);
+            this.Controls.Add(this.buttonFix);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(411, 372);
+            this.MinimumSize = new System.Drawing.Size(411, 372);
+            this.Name = "Form1";
+            this.Text = "Discord Audio Fixer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFix;
+        private System.Windows.Forms.Button buttonDefaults;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.CheckBox checkBoxFixOnStart;
+        private System.Windows.Forms.CheckBox checkBoxDefaultWhenExit;
+    }
+}
+
---
+++ Discord Audio Fixer/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Discord_Audio_Fixer
+{
+    public partial class Form1 : Form
+    {
+        private readonly string version = "0.1";
+        public Form1()
+        {
+            InitializeComponent();
+            checkBoxDefaultWhenExit.Checked = Properties.Settings.Default.DefaultsOnExit;
+            checkBoxFixOnStart.Checked = Properties.Settings.Default.FixOnStart;
+            CheckStatus();
+            if (checkBoxFixOnStart.Checked)
+            {
+                Fix();
+                WindowState = FormWindowState.Minimized;
+            }
+            Text += $" v{version}";
+        }
+
+        private void CheckStatus()
+        {
+            var affinityIntMask = GetIntMask();
+            var audioodgPath = Path.Combine(Environment.SystemDirectory, "audiodg.exe").ToLower();
+            var proccesses = Process.GetProcessesByName("audiodg");
+            bool statusDefault = false;
+            foreach (var p in proccesses)
+            {
+                p.Refresh();
+
+                if (p.MainModule.FileName.ToLower().Equals(audioodgPath))
+                {
+                    if (!p.HasExited)
+                    {
+                        statusDefault = p.PriorityClass == ProcessPriorityClass.Normal &&
+                        p.ProcessorAffinity == (IntPtr)affinityIntMask;
+
+                    }
+                }
+            }
+            if (statusDefault)
+                label.Text = "audiodg.exe are running in defaults Windows process";
+            else
+                label.Text = "audiodg.exe Fixed for Discord!";
+        }
+
+        private void Fix()
+        {
+            Random random = new Random();
+            var afinityProcessor = random.Next(0, Environment.ProcessorCount - 1);
+            var @string = "";
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                if (afinityProcessor == i)
+                    @string += "1";
+                else
+                    @string += "0";
+            }
+            var result = Convert.ToInt32(@string, 2);
+            var audioodgPath = Path.Combine(Environment.SystemDirectory, "audiodg.exe").ToLower();
+            var proccesses = Process.GetProcessesByName("audiodg");
+            foreach (var p in proccesses)
+            {
+                p.Refresh();
+
+                if (p.MainModule.FileName.ToLower().Equals(audioodgPath))
+                {
+                    if (!p.HasExited)
+                    {
+                        p.PriorityClass = ProcessPriorityClass.High;
+                        p.ProcessorAffinity = (IntPtr)result;
+                        
+                    }
+                }
+            }
+            CheckStatus();
+        }
+
+        private void SetProcessToDefault()
+        {
+            int result = GetIntMask();
+            var audioodgPath = Path.Combine(Environment.SystemDirectory, "audiodg.exe").ToLower();
+            var proccesses = Process.GetProcessesByName("audiodg");
+            foreach (var p in proccesses)
+            {
+                p.Refresh();
+
+                if (p.MainModule.FileName.ToLower().Equals(audioodgPath))
+                {
+                    if (!p.HasExited)
+                    {
+                        p.PriorityClass = ProcessPriorityClass.Normal;
+                        p.ProcessorAffinity = (IntPtr)result;
+                    }
+                }
+            }
+            CheckStatus();
+        }
+
+        private int GetIntMask()
+        {
+            var @string = "";
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+            {
+                @string += "1";
+            }
+            var result = Convert.ToInt32(@string, 2);
+            return result;
+        }
+
+
+        private void buttonFix_Click(object sender, EventArgs e)
+        {
+            Fix();
+        }
+
+        private void buttonDefaults_Click(object sender, EventArgs e)
+        {
+            SetProcessToDefault();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (checkBoxDefaultWhenExit.Checked)
+            {
+                SetProcessToDefault();
+            }
+            Properties.Settings.Default.Save();
+        }
+
+        private void checkBoxFix_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.FixOnStart = checkBoxFixOnStart.Checked;
+        }
+
+        private void checkBoxDefaultWhenExit_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DefaultsOnExit = checkBoxDefaultWhenExit.Checked;
+        }
+    }
+}
